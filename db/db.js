46655fdc5a359f19ede5b2c80e01876087e5e1d6
Glitch (poised-descriptive-jam) <none>
2022-02-26 12:35:14 +0000

    🏮🍉 Checkpoint ./db/db.js:30308062/142

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 
 const mySqlConnection = mysql.createConnection({
-    host: "sql204.epizy.com",
-    user: "epiz_31127704",
+    host: "185.27.134.212",
+    user: "id17805600_root",
     password: "iII5PcXmmcBv",
     database: "epiz_31127704_aed",
     multipleStatements: true
